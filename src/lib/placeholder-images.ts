db69948f0f58215ce9eb417eeda2708d0015dc28
Sagnik Agasti <167172754+agastisagnik2004@users.noreply.github.com>
2025-10-05 00:56:02 +0530

    Update placeholder-images.ts

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -3,7 +3,7 @@ import data from './placeholder-images.json';
 export type ImagePlaceholder = {
   id: string;
   description: string;
-  imageUrl: https://u.pcloud.link/publink/show?code=kZJJ7n5Zp9E1Em6q3AJKQrJKf0dja57iTuRy;
+  image: string 
   imageHint: string;
 };
 
